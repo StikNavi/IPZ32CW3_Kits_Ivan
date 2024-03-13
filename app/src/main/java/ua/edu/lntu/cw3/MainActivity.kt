39a271fb04872646464752c7511e1f9ce7e1fa54
StikNavi <stiknavi@gmail.com>
2024-03-13 15:11:34 +0200

    Commit 6

--- app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ua.edu.lntu.cw3.ui.theme.IPZ32CW3_Kits_IvanTheme
@@ -67,7 +68,8 @@ fun RecipeItem(recipe: Recipe) {
 
         Text(
             text = recipe.name,
-            modifier = Modifier.padding(horizontal = 120.dp)
+            style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.Bold),
+            modifier = Modifier.padding(horizontal = 16.dp)
         )
 
         if (expanded) {
@@ -78,6 +80,7 @@ fun RecipeItem(recipe: Recipe) {
         }
     }
 }
+
 
 data class Recipe(val name: String, val description: String, val imageResId: Int)
 
